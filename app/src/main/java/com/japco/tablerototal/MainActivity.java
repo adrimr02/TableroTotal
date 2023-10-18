3eea2dcbcd426ed785b801eb0f769d3cdb867ef7
Adrián Martínez <adrianmartinezr2002@gmail.com>
2023-10-19 00:21:26 +0200

    Added navigation menu

--- app/src/main/java/com/japco/tablerototal/MainActivity.java
+++ app/src/main/java/com/japco/tablerototal/MainActivity.java
@@ -1,14 +1,55 @@
 package com.japco.tablerototal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    RecordFragment recordFragment = new RecordFragment();
+    FriendsFragment friendsFragment = new FriendsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Log.i("Navigation", String.valueOf(item.getItemId()));
+            if (item.getItemId() == R.id.homeNavButton) {
+                replaceFragment(homeFragment);
+                return true;
+            } else if (item.getItemId() == R.id.recordNavButton) {
+                replaceFragment(recordFragment);
+                return true;
+            } else if (item.getItemId() == R.id.friendsNavButton) {
+                replaceFragment(friendsFragment);
+                return true;
+            }
+            return false;
+        });
+        bottomNavigationView.setSelectedItemId(R.id.homeNavButton);
+        Log.i("Navigation", String.valueOf(R.id.homeNavButton));
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, fragment);
+        fragmentTransaction.commit();
     }
 }
